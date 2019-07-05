680db38493d80fa89dd80b0fffa0892af31cceca
wusd <wsd5656126@foxmail.com>
2019-07-06 01:23:10 +0800

    010

---
+++ 010/src/main/java/com/wusd/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.wusd;
+
+import org.joda.time.DateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().write("吴盛东爱李洁,日期:" + DateTime.now().toString());
+    }
+}
---
+++ 010/src/main/java/com/wusd/LifeCycleServlet.java
@@ -0,0 +1,24 @@
+package com.wusd;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LifeCycleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Life");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
