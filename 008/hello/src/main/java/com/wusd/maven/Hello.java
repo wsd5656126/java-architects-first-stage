3d5e4af7200b47f1c111cc847604f550d8f8e915
wsd <wsd5656126@foxmail.com>
2019-07-05 18:11:03 +0800

    008

---
+++ 008/hello/src/main/java/com/wusd/maven/Hello.java
@@ -0,0 +1,12 @@
+package com.wusd.maven;
+
+/**
+ * 1.mvn compile
+ * 2.mvn clean
+*  3.mvn test
+ */
+public class Hello {
+    public void hello() {
+        System.out.println("dong love jie");
+    }
+}
---
+++ 008/hellofriend/src/main/java/com/wusd/HelloFriend.java
@@ -0,0 +1,10 @@
+package com.wusd;
+
+import com.wusd.maven.Hello;
+
+public class HelloFriend {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.hello();
+    }
+}
