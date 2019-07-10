1849714021b5d71e22cd9298fd8a2c1c84b901d9
wsd <wsd5656126@foxmail.com>
2019-07-10 19:24:19 +0800

    016

---
+++ 016/016_entity/src/main/java/com/wusd/entity/VideoType.java
@@ -0,0 +1,7 @@
+package com.wusd.entity;
+
+@Data
+public class VideoType {
+    private Integer id;
+    private String typeName;
+}
---
+++ 016/016_web/src/main/java/com/wusd/controller/TestController.java
@@ -0,0 +1,4 @@
+package com.wusd.controller;
+
+public class TestController {
+}
